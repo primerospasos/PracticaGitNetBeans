4551d3e51cef81ba855d07652020e64a145355a3
a15oscarlp <a15oscarlp@a21eq07.sanclemente.local>
2016-05-11 19:45:18 +0200

    System out 2

--- JavaApplication5/src/javaapplication5/JavaApplication5.java
+++ JavaApplication5/src/javaapplication5/JavaApplication5.java
@@ -17,6 +17,7 @@ public class JavaApplication5 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("hola mundo");
+        System.out.println("otro hola mundo");
     }
     
 }
